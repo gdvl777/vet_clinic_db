60e822ebd5363adedc6939e32d6be2ad804779c0
gdvl777 <gdvl777@gmail.com>
2023-10-25 23:46:06 -0500

    query

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(5, 2)
 );
+
+/* Update animals table */
+
+ALTER TABLE animals ADD species VARCHAR(50);
